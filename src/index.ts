3f1316dc7ced44e519fadd7027bd9e5194f01560
Alec Armbruster <35377827+alectrocute@users.noreply.github.com>
2025-07-11 13:13:45 -0700

    bugfix: properly export ts types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { getLoopbackAudioMediaStream } from './renderer.js';
 import { initMain } from './main.js';
 
+// TypeScript needs to know about all exports
+export { getLoopbackAudioMediaStream, initMain };
+
+// Runtime conditional export based on Electron context
 if (process.type === 'renderer') {
-    module.exports.getLoopbackAudioMediaStream = getLoopbackAudioMediaStream;
+    module.exports = { getLoopbackAudioMediaStream };
 } else {
-    module.exports.initMain = initMain;
+    module.exports = { initMain };
 }
